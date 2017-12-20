ae8b015f93f3997c11a56523808876bad2284b2c
DefaultStudent <comrade_vodka@outlook.com>
2017-12-20 18:36:28 +0800

    配置

--- src/com/wbs/customer/vo/Customer.java
+++ src/com/wbs/customer/vo/Customer.java
@@ -1,6 +1,7 @@
 package com.wbs.customer.vo;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -9,9 +10,70 @@ import javax.persistence.Table;
  */
 
 @Entity
-@Table(name = "Customers")
+@Table(name="Customers")
 public class Customer {
-    private int cusId;
+    private String cusId;
     private String cusAlias;
     private String cusPwd;
+    private String cusName;
+    private String cusTel;
+    private String cusGender;
+    private String cusBirth;
+
+    @Id
+    public String getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(String cusId) {
+        this.cusId = cusId;
+    }
+
+    public String getCusAlias() {
+        return cusAlias;
+    }
+
+    public void setCusAlias(String cusAlias) {
+        this.cusAlias = cusAlias;
+    }
+
+    public String getCusPwd() {
+        return cusPwd;
+    }
+
+    public void setCusPwd(String cusPwd) {
+        this.cusPwd = cusPwd;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public String getCusTel() {
+        return cusTel;
+    }
+
+    public void setCusTel(String cusTel) {
+        this.cusTel = cusTel;
+    }
+
+    public String getCusGender() {
+        return cusGender;
+    }
+
+    public void setCusGender(String cusGender) {
+        this.cusGender = cusGender;
+    }
+
+    public String getCusBirth() {
+        return cusBirth;
+    }
+
+    public void setCusBirth(String cusBirth) {
+        this.cusBirth = cusBirth;
+    }
 }
