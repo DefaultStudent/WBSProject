0a5bf0b7bb75f96a06a86c53a38c6ee528662c3f
DefaultStudent <comrade_vodka@outlook.com>
2017-12-21 13:53:44 +0800

    修改查询全部方法

--- src/com/wbs/bookinfo/dao/BookInfoDao.java
+++ src/com/wbs/bookinfo/dao/BookInfoDao.java
@@ -1,27 +1,22 @@
 package com.wbs.bookinfo.dao;
 
 import com.wbs.bookinfo.vo.BookInfo;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.springframework.orm.hibernate4.HibernateCallback;
+
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
-import javax.management.Query;
 import java.util.List;
 
 /**
- * @author
+ * @author Simon
  * @version 1.0 2017-12-21
  */
 
 public class BookInfoDao extends HibernateDaoSupport{
-
     /**
-     * 查询全部图书信息
-     * @param bookInfo
+     *
      * @return
      */
-    public List<BookInfo> findAllBookInfo(BookInfo bookInfo){
+    public List<BookInfo> findAllBookInfo(){
         String hql = "from BookInfo";
         List<BookInfo> list = (List<BookInfo>) this.getHibernateTemplate().find(hql);
         if (list != null && list.size() > 0) {
